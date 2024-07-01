a566ff182e3e618d5ad5a37ecaf4c1405225918e
Яхонтов Григорий <g.yakhontov@bftcom.com>
2024-07-01 18:48:36 +0300

    fix: exercise_1 link

--- lesson_7/exercise_1.py
+++ lesson_7/exercise_1.py
@@ -1,4 +1,4 @@
-# lesson_7 exercise 1 https://lms.synergy.ru/practicums/attempt/5025987/?groupPeriodId=1166613
+# lesson_7 exercise 1 https://lms.synergy.ru/practicums/attempt/5025989/1?groupPeriodId=1166613
 
 s =  input()
 print('yes' if s == s[::-1] else 'no')
