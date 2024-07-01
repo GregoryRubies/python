d339f4cfa6d2470fab7c5522d1e97c382a1664b8
Яхонтов Григорий <g.yakhontov@bftcom.com>
2024-07-01 14:44:27 +0300

    feat:   lesson 4

---
+++ lesson_4/exercise_1.py
@@ -0,0 +1,8 @@
+# lesson_4 exercise 1 https://lms.synergy.ru/practicums/attempt/5025983/?groupPeriodId=1166613
+
+print('Введите стороны прямоугольника!')
+print('Сторона 1:', end = ' ')
+h = float(input())
+print('Сторона 2:', end = ' ')
+w = float(input())
+print("Периметр = %.2f;\nПлощадь = %.2f;"%(2 * (h + w), h * w))
