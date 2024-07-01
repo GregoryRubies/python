aecfe5461f4a12ad2ae3701ffa2e886202028608
Яхонтов Григорий <g.yakhontov@bftcom.com>
2024-07-01 17:01:24 +0300

    feat: exercise_1 of lesson_6

---
+++ lesson_6/exercise_1.py
@@ -0,0 +1,6 @@
+# lesson_6 exercise 1 https://lms.synergy.ru/practicums/attempt/5025987/?groupPeriodId=1166613
+
+r = 0
+for i in [int(input()) for n in range(int(input()))]:
+  if i == 0: r += 1
+print(f'Значение 0 введено {r} раз')
