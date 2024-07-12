ed2e83d07f3ee447b65d61ddcc740ccae4f82768
GregoryRubies <gregoryrubies@gmail.com>
2024-07-12 22:01:44 +0300

    feat: exercise_2 of lesson_15

--- lesson_15/classes.py
+++ lesson_15/classes.py
@@ -3,3 +3,9 @@ class Transport:
     self.name = name
     self.max_speed = max_speed
     self.mileage = mileage
+  def seating_capacity(self, capacity):
+    return f"Вместимость одного автобуса {self.name}  {capacity} пассажиров"
+  
+class Autobus(Transport):
+  def seating_capacity(self, capacity=50):
+    return f"Вместимость одного автобуса {self.name}  {capacity} пассажиров"
---
+++ lesson_15/exercise_2.py
@@ -0,0 +1,7 @@
+# lesson_15 exercise 2 https://lms.synergy.ru/practicums/attempt/5026003/1?groupPeriodId=1166613
+
+from classes import Autobus
+
+bus:Autobus = Autobus('Renaul Logan', 180, 12)
+
+print(bus.seating_capacity())
